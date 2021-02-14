a9e4f00c36bff0c3eb2238314e3469d36dca24fd
David LeGare <dlegare.1001@gmail.com>
2021-02-14 14:42:39 -0600

    Add support for async drawing gizmos when object is selected
    This allows `GizmosPlusAsync` to mirror the functionality provided by the built-in `OnDrawGizmosSelected()`.

--- Scripts/GizmosPlusAsync.cs
+++ Scripts/GizmosPlusAsync.cs
@@ -1,4 +1,3 @@
-
 using System;
 using UnityEngine;
 
@@ -8,6 +7,16 @@ namespace Zchfvy.Plus {
     /// </summary>
     public static class GizmosPlusAsync {
         private static GizmosPlusAsyncDrawer drawer;
+
+        private static GizmosPlusAsyncDrawer GetOrCreateDrawer() {
+            if (drawer == null) {
+                var go = new GameObject("_GizmosPlusAsyncDrawer");
+                drawer = go.AddComponent<GizmosPlusAsyncDrawer>();
+            }
+
+            return drawer;
+        }
+
         /// <summary>
         /// Allows drawing of Gizmos outside of OnDrawGizmos flow
         /// </summary>
@@ -24,11 +33,15 @@ namespace Zchfvy.Plus {
         /// </code>
         /// </example>
         public static void DrawAsync(Action drawFunc) {
-            if (drawer == null) {
-                var go = new GameObject("_GizmosPlusAsyncDrawer");
-                drawer = go.AddComponent<GizmosPlusAsyncDrawer>();
-            }
-            drawer.Enqueue(drawFunc);
+            GetOrCreateDrawer().Enqueue(drawFunc);
+        }
+
+        public static void DrawSelectedAsync(this GameObject gameObject, Action drawFunc) {
+            GetOrCreateDrawer().EnqueueSelected(gameObject, drawFunc);
+        }
+
+        public static void DrawSelectedAsync<T>(this T component, Action drawFunc) where T : Component {
+            DrawSelectedAsync(component.gameObject, drawFunc);
         }
     }
 }
--- Scripts/GizmosPlusAsyncDrawer.cs
+++ Scripts/GizmosPlusAsyncDrawer.cs
@@ -9,15 +9,30 @@ namespace Zchfvy.Plus {
     /// </summary>
     public class GizmosPlusAsyncDrawer : MonoBehaviour {
         private Queue<Action> drawQueue = new Queue<Action>();
+        private Queue<(GameObject, Action)> drawSelectedQueue = new Queue<(GameObject, Action)>();
 
         public void Enqueue(Action newItem) {
             drawQueue.Enqueue(newItem);
+        }
+
+        public void EnqueueSelected(GameObject gameObject, Action newItem) {
+            drawSelectedQueue.Enqueue((gameObject, newItem));
         }
 
         void OnDrawGizmos() {
             while (drawQueue.Count > 0) {
                 var act = drawQueue.Dequeue();
                 act();
+            }
+
+            while (drawSelectedQueue.Count > 0) {
+                var (gameObject, act) = drawSelectedQueue.Dequeue();
+
+#if UNITY_EDITOR
+                if (UnityEditor.Selection.Contains(gameObject.GetInstanceID())) {
+                    act();
+                }
+#endif
             }
         }
     }
