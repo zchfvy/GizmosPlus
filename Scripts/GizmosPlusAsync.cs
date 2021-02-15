ce1dc4d182c22ac790d201493561014d343348ae
David LeGare <dlegare.1001@gmail.com>
2021-02-15 15:10:51 -0600

    Add documentation for OnDrawSelectedAsync()

--- Scripts/GizmosPlusAsync.cs
+++ Scripts/GizmosPlusAsync.cs
@@ -36,10 +36,52 @@ namespace Zchfvy.Plus {
             GetOrCreateDrawer().Enqueue(drawFunc);
         }
 
+        /// <summary>
+        /// Performs the provided draw logic when the specified game object is selected in the
+        /// scene view.
+        /// </summary>
+        ///
+        /// <param name="gameObject">
+        /// The object that must be selected in order for the gizmos to be drawn.
+        /// </param>
+        /// <param name="drawFunc">
+        /// A lambda expression that performs the gizmo drawing logic.
+        /// </param>
+        ///
+        /// <example>
+        /// In some main thread function other than <c>OnDrawGizmosSelected()</c>:
+        /// <code>
+        /// GizmosPlusAsync.DrawSelectedAsync(targetObject, () => {
+        ///     Gizmos.DrawLine(...);
+        ///     GizmosPlus.DrawCross(...);
+        /// });
+        /// </code>
+        /// </example>
         public static void DrawSelectedAsync(this GameObject gameObject, Action drawFunc) {
             GetOrCreateDrawer().EnqueueSelected(gameObject, drawFunc);
         }
 
+        /// <summary>
+        /// Performs the provided draw logic when the specified component is selected in the
+        /// scene view.
+        /// </summary>
+        ///
+        /// <param name="component">
+        /// A component on the object that must be selected in order for the gizmos to be drawn.
+        /// </param>
+        /// <param name="drawFunc">
+        /// A lambda expression that performs the gizmo drawing logic.
+        /// </param>
+        ///
+        /// <example>
+        /// In some main thread function other than <c>OnDrawGizmosSelected()</c>:
+        /// <code>
+        /// GizmosPlusAsync.DrawSelectedAsync(this, () => {
+        ///     Gizmos.DrawLine(...);
+        ///     GizmosPlus.DrawCross(...);
+        /// });
+        /// </code>
+        /// </example>
         public static void DrawSelectedAsync<T>(this T component, Action drawFunc) where T : Component {
             DrawSelectedAsync(component.gameObject, drawFunc);
         }
